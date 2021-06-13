0cd73c613d5d5574c1df3d54537953512b50889e
Sahil Masand <masand.sahil@outlook.com>
2021-06-13 21:11:55 +0800

    Removed personal Glassnode API key

--- run.py
+++ run.py
@@ -20,8 +20,11 @@ if __name__ == '__main__':
     creds_file = args.creds if args.creds else DEFAULT_CREDS_FILE
     parsed_creds = load_config(creds_file)
     access_key, secret_key = load_correct_creds(parsed_creds)
+    
+    # Glassnode API Key. Sign up for free here: https://glassnode.com/
+    GLASSNODE_KEY = 'xx'
 
     # Create a new bot
-    bot = DeepQBot(access_key, secret_key, GLASSNODE_KEY='1ttU5pGaSi3qW894pJcAmJlMICf')
+    bot = DeepQBot(access_key, secret_key, GLASSNODE_KEY)
     #bot.run_bot()
     bot.trainNetwork()
